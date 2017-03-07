5b8ae8119beb606c214231ee3d883b5a4762154c
davidhenley <henleydr@gmail.com>
2017-03-06 19:24:45 -0600

    lodash examples

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ console.log('Starting app.js');
 
 // const fs = require('fs');
 // const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes');
 
-console.log('Result:', notes.add(19, 23));
+// console.log(_.isString(true)); // false
+// console.log(_.isString('David')); // true
+
+let filteredArray = _.uniq(['David', 1, 'David', 1, 2, 3, 4]);
+console.log(filteredArray);
 
 /*
+// File System and OS Node modules
 let user = os.userInfo();
 
 fs.appendFile('greetings.txt', `Hello ${user.username}!`, (err) => {
