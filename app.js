3d6f997c8f6c65161853b4c3ca1032b1f2fb218e
davidhenley <henleydr@gmail.com>
2017-03-06 19:51:50 -0600

    Command switch

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-console.log('Starting app.js');
+// console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes');
+
+const command = process.argv[2];
+console.log('Command:', command);
+
+switch (command) {
+  case 'add':
+    console.log('Adding new note');
+    break;
+  case 'list':
+    console.log('Listing all notes');
+    break;
+  case 'read':
+    console.log('Reading individual note');
+    break;
+  case 'delete':
+    console.log('Deleting note');
+    break;
+  default:
+    console.log('Command not recognized');
+}
